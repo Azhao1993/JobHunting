c530677d5f0f333b12a7b05eb834d60cd1527864
andrew <1124913956@qq.com>
2019-03-17 16:18:34 +0800

    字节跳动2019春招第一批

--- ByteDance/ByteDance_2019_BackgroundDevelopment_1/Cions.java
+++ ByteDance/ByteDance_2019_BackgroundDevelopment_1/Cions.java
@@ -1,6 +1,7 @@
 package ByteDance_2019_BackgroundDevelopment_1;
 
 import java.util.Scanner;
+
 /*
 	第一题:
 		某国货币系统包含面值1元，4元，16元，64元共计4钟硬币，以及面值1024元的纸币。
@@ -31,6 +32,30 @@ public class Cions {
 		}
 		System.out.println(result);
 
+	}
+	//牛客
+	public static void main0(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int result64 = 0;
+		int result16 = 0;
+		int result4 = 0;
+		int total = 0;
+		N = 1024 - N;
+		while (N > 64) {
+			result64 += N >> 6;
+			N -= result64 * 64;
+		}
+		while (N > 16) {
+			result16 += N >> 4;
+			N -= result16 * 16;
+		}
+		while (N > 4) {
+			result4 += N >> 2;
+			N -= result4 * 4;
+		}
+		total = result64 + result16 + result4 + N;
+		System.out.println(total);
 	}
 
 }
