3aedbbedb9835eb4af5f2f6152a078b20a15b377
Andrew <Andrew@219.216.65.150>
2019-04-11 20:47:26 +0800

    webank

--- WeBank/WeBank_2020_Spring/Main3.java
+++ WeBank/WeBank_2020_Spring/Main3.java
@@ -6,10 +6,15 @@ public class Main3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		long n = sc.nextInt();
-		long k = sc.nextInt();
-		long m = sc.nextInt();
-		long res = n*k/m;
+		long n = sc.nextInt();// 兽
+		long k = sc.nextInt();// 技能
+		long m = sc.nextInt();// 闲先知
+		long res = 0;
+		if (m <= n) {
+			res = (n * k + m - 1) / m;
+		}else {
+			res = k;
+		}
 		System.out.println(res);
 	}
 
