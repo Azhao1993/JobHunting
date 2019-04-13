7d761df796e9ffc0f793a768f1c28320392283e0
andrew <1124913956@qq.com>
2019-04-13 18:14:53 +0800

    阿里模拟题

--- Ailibaba/Ailibaba_2020_test/Main.java
+++ Ailibaba/Ailibaba_2020_test/Main.java
@@ -59,35 +59,9 @@ public class Main {
 	/**
 	 * 当然，你也可以不按照这个模板来作答，完全按照自己的想法来
 	 **/
-	public static String measureDistance(Point2D.Double point, List<Point2D.Double> pts) {		
-		if(IsPtInPoly(point, pts)) {
-			return "yes,0";
-		}else {
-			
-		}
-	}
+	
 
-	public static void main2(String[] args) {
-		Scanner in = new Scanner(System.in);
-		String line = in.nextLine();
-		// (x,y)为小广所在的位置
-		double x = Double.parseDouble(line.split(",")[0]);
-		double y = Double.parseDouble(line.split(",")[1]);
-
-		line = in.nextLine();
-		// xList记录了多边形n个点的x坐标,yList记录了多边形n个点的y坐标
-		List<Double> xList = new ArrayList<>();
-		List<Double> yList = new ArrayList<>();
-		String[] array = line.split(",");
-		for (int i = 0; i < array.length; i++) {
-			xList.add(Double.parseDouble(array[i]));
-			yList.add(Double.parseDouble(array[i + 1]));
-			i++;
-		}
-		in.close();
-		System.out.println(measureDistance(xList, yList, x, y));
-	}
-
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String line = in.nextLine();
