3dd4572d9915db20fb335a6b68d4df9387973f99
Andrew <Andrew@202.199.6.245>
2019-04-28 21:36:17 +0800

    度

--- duxiaoman/Main.java
+++ duxiaoman/Main.java
@@ -1,5 +1,44 @@
 package duxiaoman;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0;i<t;i++) {
+			long temp = sc.nextLong();
+			sloution(temp);
+		}
+	}
+
+	private static void sloution(long temp) {
+		if(temp<10) {
+			System.out.println("G");
+			return;
+		}
+		boolean flag = false;
+		long n = temp;
+		while(n>0) {
+			long num = n%10;
+			n /= 10;
+			if(num==0) {
+				flag = true;
+				continue;
+			}
+			if(temp%num==0) {
+				flag = true;				
+			}else if(flag){
+				System.out.println("H");
+				return;
+			}			
+		}
+		if(!flag) {
+			System.out.println("S");
+		}else {
+			System.out.println("G");
+		}
+		
+		
+	}
 }
--- duxiaoman/Main2.java
+++ duxiaoman/Main2.java
@@ -1,10 +1,23 @@
 package duxiaoman;
 
+import java.util.Scanner;
+
 public class Main2 {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] A = new int[n];
+		for(int i = 0;i<n;i++) {
+			A[i] = sc.nextInt();
+		}
+		sloution(n,A);
 
+	}
+
+	private static void sloution(int n, int[] a) {		
+		int min = a[0];
+		
 	}
 
 }
